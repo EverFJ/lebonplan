d2d546a7c606f363033a72446645a297cab6b501
hasnaine <hasnaineali@icloud.com>
2021-10-25 17:28:29 +0200

    commit

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="registerconteiner">
+        <h1 className="Signup">Signup</h1>
+        <form className="Signup-register">
+        <input
+          className="TextInput"
+          type="text"
+          name="name"
+          placeholder="Name"
+        />
+      </form>
+      </div>
     </div>
   );
 }
