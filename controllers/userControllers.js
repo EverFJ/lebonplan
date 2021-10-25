c9a51e6556b070bfcbdafa2155a91760ac7c4a3a
Francois Janson <jansonfrancois@gmail.com>
2021-10-25 15:42:52 +0200

    users routes and controllers done

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,2 +1,36 @@
 const mongoose = require("mongoose")
 const User = require("../models/User")
+
+const getUserPage = (req, res) => {
+    User.findOne({
+        _id: req.params.id
+    })
+}
+const handleSignup = (req, res) => {
+    const user = new User({
+        ...req.body
+    })
+    user.save()
+        .then(user => {
+            res.status(200).json({
+                message: "User created ! ",
+                user: user
+            })
+        })
+        .catch(err => {
+            console.error(err)
+            res.status(500).json({
+                message: "User not created !",
+                error: err
+            })
+        })
+}
+const handleLogin = (req, res) => {
+
+}
+
+module.exports = {
+    getUserPage,
+    handleSignup,
+    handleLogin
+}
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const UsersControllers = require("../controllers/userControllers")
 
 router.get("/:id", UsersControllers.getUserPage)
+router.post("/signup", UsersControllers.handleSignup)
+router.post("/login", UsersControllers.handleLogin)
 
 module.exports = router
