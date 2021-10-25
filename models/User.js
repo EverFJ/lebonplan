3a7aae739cf1ea325be7d65c3a69f20231bb6ff7
Fatoumata86 <soukounafatoumata86@gmail.com>
2021-10-25 16:41:22 +0200

    modification models

--- models/User.js
+++ models/User.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     surName: String,
     firstName: String,
-    userName: String,
-    email:{type : String, unique: true, trim: true, lowercase: true},
-    password: String,
+    userName: {type : String , required: true},
+    email:{type : String, unique: true, trim: true, lowercase: true, required: true},
+    password:{type : String , required: true},
     image: String,
 });
 
