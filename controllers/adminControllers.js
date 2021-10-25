b8e296714bdbc8e5b61a62884ba9234e6e54128f
Francois Janson <jansonfrancois@gmail.com>
2021-10-25 16:37:03 +0200

    productControllers update

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -2,9 +2,41 @@ const mongoose = require("mongoose")
 const Product = require("../models/Product")
 
 const createProduct = (req, res) => {
+    const product = new Product({
+        ...req.body
+    })
+    product.save()
+        .then(product => {
+            res.status(200).json({
+                message: "Product created !",
+                product: product
+            })
+        })
+        .catch(err => {
+            console.error(err)
+            res.status(500).json({
+                message: "Error while creating product !",
+                error: err
+            })
+        })
 
 }
 const getProductPage = (req, res) => {
+    Product.findOne({
+            _id: req.params.id
+        })
+        .then(product => {
+            res.status(200).json({
+                product: product
+            })
+        })
+        .catch(err => {
+            console.error(err)
+            res.status(500).json({
+                error: err
+            })
+        })
+
 
 }
 const updateProduct = (req, res) => {
