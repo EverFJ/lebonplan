bba470f7bd1a99f562f4537c202dad04740a0ed1
Francois Janson <jansonfrancois@gmail.com>
2021-10-25 13:48:46 +0200

    structuring folders

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: false
+}))
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
