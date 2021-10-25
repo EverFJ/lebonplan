a079806b1b5885031f84807a26e4335cfeacb555
Francois Janson <jansonfrancois@gmail.com>
2021-10-25 15:28:37 +0200

    admin routes

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,10 +1,35 @@
 const mongoose = require("mongoose")
 
 const getAllProducts = (req, res) => {
+    Product.find()
+        .then(products => {
+            res.status(200).json({
+                products
+            })
+        })
+        .catch(err => {
+            console.error(err)
+            res.status(500).json({
+                err
+            })
+        })
 
 }
 const getOneProduct = (req, res) => {
-
+    Product.findOne({
+            _id: req.params.id
+        })
+        .then(product => {
+            res.status(200).json({
+                product
+            })
+        })
+        .catch(err => {
+            console.error(err)
+            res.status(500).json({
+                err
+            })
+        })
 }
 
 module.exports = {
