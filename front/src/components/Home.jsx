c994bfbc629e95c7799c9711e5564bee37ad4292
Francois Janson <jansonfrancois@gmail.com>
2021-10-26 16:39:05 +0200

    adding search form

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -12,6 +12,43 @@ export default function Home(props) {
   return (
     <>
       <h1>Home</h1>
+      <form action="/products" method="post">
+        <div className="row m-4">
+          <div className="col-4">
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              placeholder="Search for a particular product"
+            />
+          </div>
+          <div className="col-3">
+            <select
+              className="form-select"
+              aria-label="select your city"
+              name="city"
+            >
+              <option value="" disabled>
+                Select your city
+              </option>
+              <option value="paris">Paris</option>
+              <option value="lyon">Lyon</option>
+              <option value="marseille">Marseille</option>
+            </select>
+          </div>
+          <div className="col-3">
+            <input
+              className="form-control"
+              type="text"
+              name="price"
+              placeholder="Enter your maximum price"
+            />
+            <button className="btn btn-primary m-4" type="submit">
+              Search
+            </button>
+          </div>
+        </div>
+      </form>
       <p>{products.length} produits en vente</p>
       <ul>
         {products.length !== 0 &&
