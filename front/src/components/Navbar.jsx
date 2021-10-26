5361c9275e4811bd14106a0c65f073e8af753504
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-10-26 15:58:38 +0200

    navbar

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import bootstrap from "bootstrap";
 
 export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="#">
+          <Link className="navbar-brand" to="/">
             Le Bon Plan
           </Link>
           <button
@@ -23,27 +24,27 @@ export default class Navbar extends Component {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link className="nav-link" to="/">
+                <Link className="nav-link" to="/Home">
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/users/login">
+                <Link className="nav-link" to="/Login">
                   Login
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/users/">
+                <Link className="nav-link" to="/Profil">
                   Profil
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/admin">
+                <Link className="nav-link" to="/Admin">
                   Admin
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/users/signup">
+                <Link className="nav-link" to="/Signup">
                   Signup
                 </Link>
               </li>
