c2371830645996c7873fae1c8cb03a4b7720ac8f
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-10-26 10:31:22 +0200

    setup main-side

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Home from "../components/Home";
-import Login from "../components/Login";
-import Admin from "../components/Admin";
-import Profil from "../components/Profil";
-import Signup from "../components/Signup";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Admin from "./components/Admin";
+import Profil from "./components/Profil";
+import Signup from "./components/Signup";
+import Error404 from "./components/Error404";
 
 function App() {
   return (
