ca64aa39cb6fb8faf6f6a8111210000262df0dab
Francois Janson <jansonfrancois@gmail.com>
2021-10-26 14:38:04 +0200

    addind bootstrap

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
-// import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar";
 import Login from "./components/Login";
 import Admin from "./components/Admin";
 import Profil from "./components/Profil";
@@ -11,7 +11,7 @@ import Error404 from "./components/Error404";
 function App() {
   return (
     <BrowserRouter>
-      {/* <NavBar /> */}
+      <NavBar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/users/login" component={Login} />
