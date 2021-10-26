45302b9150b61ae21b45c7a6c83ee1d03ab36ee6
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-10-26 16:05:18 +0200

    fix npm vulnerabilities

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import bootstrap from "bootstrap";
 
 export default class Navbar extends Component {
   render() {
