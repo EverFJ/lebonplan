f2386b0f730e8342c8d78b9d4028410d1cce51e2
Francois Janson <jansonfrancois@gmail.com>
2021-10-26 14:04:37 +0200

    merging

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,10 @@ const router = express.Router()
 const productsControllers = require("../controllers/productsController")
 
 router.get("/", productsControllers.getAllProducts)
+// Adding query params
+
 router.get("/:id", productsControllers.getOneProduct)
 router.get("/cities/:city", productsControllers.getProductsFromCity)
+router.get("/name/:name", productsControllers.getProductsFromName)
 
 module.exports = router
