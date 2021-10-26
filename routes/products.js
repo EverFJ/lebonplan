911ae39fa817c213f092755f9965bd315fbb18ef
Francois Janson <jansonfrancois@gmail.com>
2021-10-26 14:18:13 +0200

    cleaning

--- routes/products.js
+++ routes/products.js
@@ -2,20 +2,11 @@ const express = require("express")
 const router = express.Router()
 const productsControllers = require("../controllers/productsController")
 
-<<<<<<< HEAD
-router.get("/", productsControllers.getAllProducts)
+router.get("/", productsControllers.getSearchedProducts)
 // Adding query params
 
 router.get("/:id", productsControllers.getOneProduct)
 router.get("/cities/:city", productsControllers.getProductsFromCity)
 router.get("/name/:name", productsControllers.getProductsFromName)
-=======
-// router.get("/", productsControllers.getSearchedProducts)
-// Adding query params
-
-router.get("/:id", productsControllers.getOneProduct)
-// router.get("/cities/:city", productsControllers.getProductsFromCity)
-// router.get("/name/:name", productsControllers.getProductsFromName)
->>>>>>> 4e8666957115f07aeb8f27365f670a464677238e
 
 module.exports = router
