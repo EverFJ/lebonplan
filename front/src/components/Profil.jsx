2e29698f05f36962d2de0257f27504785580b75c
Francois Janson <jansonfrancois@gmail.com>
2021-10-27 14:33:09 +0200

    updating profile.jsx

--- front/src/components/Profil.jsx
+++ front/src/components/Profil.jsx
@@ -1,11 +1,27 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
-export default class Profil extends Component {
-  render() {
-    return (
-      <div className="Profil">
-        <h1>Profil</h1>
-      </div>
-    );
-  }
+export default function Profil(props) {
+  const [user, setUser] = useState([]);
+  const id = props.match.params.id;
+  const apiUrl = "http://localhost:8000";
+
+  useEffect(() => {
+    fetch(apiUrl + "/products/" + id)
+      .then(res => res.json())
+      .then(data => setUser(data));
+  }, []);
+
+  console.log(`PROFILE.JSX user`, user);
+  const { firstName, surName, userName, email, profileImage } = user;
+  return (
+    <div>
+      <h1>Profil</h1>
+      <h2>
+        {firstName} {surName}
+      </h2>
+      <p>Username: {userName}</p>
+      <p>email: {email}</p>
+      <img src={apiUrl + profileImage} alt={`${userName} picture`} />
+    </div>
+  );
 }
